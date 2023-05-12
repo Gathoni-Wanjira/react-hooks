9813ba6393c004bfa355723c6a001b653988df6c
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-05-12 10:50:23 +0300

    useState IncreaseCount

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+
+
+  const increaseCounter = () => {
+    setCounter(counter + 1)
+  }
+  console.log(counter);
+
+
+
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
-    
+      <h1>Counter:{counter}</h1>
+      <button onClick={increaseCounter}> Increase </button>
+
     </div>
   );
 }
 
 export default App;
+
+
+// NOTES TO REMEMBER
+// UseState hooks are functions used  to add State in a functional component.
+// "State" are just values or variables of a component. 
+// To use any hook ... you first need to import that hook from react library.
